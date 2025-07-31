a8fec60cf4ed2c6b4afd4655c40363e80970517a
Jen Kelly <jen.kelly@powerhrg.com>
2025-07-31 11:28:54 -0400

    Create give_freebie method

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,15 @@
 class Company < ActiveRecord::Base
   has_many :freebies
   has_many :devs, through: :freebies
+  
+  def give_freebie(dev, item_name, value)
+    Freebie.create(item_name: item_name, value: value, company_id: id, dev_id: dev.id )
+  end
+    
+    # Company.oldest_company
+    #     returns the Company instance with the earliest founding year
+
+
+
+
 end

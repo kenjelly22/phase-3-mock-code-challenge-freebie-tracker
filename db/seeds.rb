4898b27b9db4e9c689795a8d2de755d569944545
Jen Kelly <jen.kelly@powerhrg.com>
2025-07-31 10:50:10 -0400

    Write method to return string in freebie

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,20 @@
-puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+# puts "Creating companies..."
+# Company.create(name: "Google", founding_year: 1998)
+# Company.create(name: "Facebook", founding_year: 2004)
+# Company.create(name: "Dunder Mifflin", founding_year: 2002)
+# Company.create(name: "Enron", founding_year: 1995)
 
-puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
-
+# puts "Creating devs..."
+# Dev.create(name: "Rick")
+# Dev.create(name: "Morty")
+# Dev.create(name: "Mr. Meseeks")
+# Dev.create(name: "Gazorpazop")
+t
 puts "Creating freebies..."
 
-# ***************************************************************
-# * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
+Freebie.create(item_name: "Hoodie", value: 60, company_id: Company.first.id, dev_id: Dev.first.id)
+Freebie.create(item_name: "Pen", value: 10, company_id: Company.second.id, dev_id: Dev.second.id)
+Freebie.create(item_name: "Notebook", value: 20, company_id: Company.third.id, dev_id: Dev.third.id)
+Freebie.create(item_name: "Socks", value: 10, company_id: Company.fourth.id, dev_id: Dev.fourth.id)
 
 puts "Seeding done!"

80b1d37c5b48b90d1e982db0a7bc6c66b7bdd5b1
Jen Kelly <jen.kelly@powerhrg.com>
2025-08-01 11:25:33 -0400

    Create received_one? method

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -2,4 +2,8 @@ class Dev < ActiveRecord::Base
   has_many :freebies
   has_many :companies, through: :freebies
 
+  def received_one?(item_name)
+    freebies.where(item_name: item_name).exists?
+  end
+
 end

24d801e9e146f5e291e51c0743c0de50acad3dfc
Jen Kelly <jen.kelly@powerhrg.com>
2025-08-01 08:43:27 -0400

    Complete lab

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -5,7 +5,22 @@ class Dev < ActiveRecord::Base
     def received_one?(item_name)
       return true if freebies.find_by(item_name: item_name)
       return false
+
+      # if freebies.find_by(item_name: item_name)
+      #   true
+      # else
+      #   false
+      # end
+
+      # freebies.pluck(:item_name).any?(item_name)
     end
+
+    # Dev#give_away(dev, freebie)
+    #     accepts a Dev instance and a Freebie instance,
+    #      changes the freebie's dev to be the given dev;
+    #       your code should only make the change if the 
+    #       freebie belongs to the dev who's giving it 
+    #       away
 
     def give_away(new_dev, freebie)
       if id == freebie.dev.id

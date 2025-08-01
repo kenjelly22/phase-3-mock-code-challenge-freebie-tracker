34ecb0380645c8635ae6997609ece347d87111c5
Jen Kelly <jen.kelly@powerhrg.com>
2025-08-01 12:07:57 -0400

    Create give_away method

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -3,7 +3,15 @@ class Dev < ActiveRecord::Base
   has_many :companies, through: :freebies
 
   def received_one?(item_name)
-    freebies.where(item_name: item_name).exists?
+    freebies.exists?(item_name: item_name)
+  end
+
+  def give_away(new_dev, freebie)
+    if freebies.include?(freebie)
+      freebie.update(dev_id: new_dev.id)
+    else
+      "Hey! That's not yours!"
+    end
   end
 
 end
